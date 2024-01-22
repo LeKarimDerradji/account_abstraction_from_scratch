47bd91c8da3abec9d9fd80ea21ad97e7c6fd7c8a
LeKarimDerradji <karim.derradji.pro@gmail.com>
2024-01-22 20:02:33 +0100

    validateUserOp_1

--- contracts/EntryPoint.sol
+++ contracts/EntryPoint.sol
@@ -3,3 +3,12 @@ pragma solidity ^0.8.9;
 
 import "@account-abstraction/contracts/core/EntryPoint.sol";
 
+contract Account is IAccount {
+    function validateUserOp(
+        UserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData) {
+        return 0;
+    }
+}
