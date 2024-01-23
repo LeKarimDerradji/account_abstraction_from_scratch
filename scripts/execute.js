50c8153e0583523b42791bd0d9f4d9480eb0c6cd
LeKarimDerradji <karim.derradji.pro@gmail.com>
2024-01-23 12:08:02 +0100

    userOp_construct

---
+++ scripts/execute.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const ep = await hre.ethers.deployContract("EntryPoint");
+
+  const userOp = {
+    sender,
+    nonce,
+    initCode,
+    callData,
+    callGasLimit: 200_000,
+    verificationGasLimit: 200_000,
+    preVerificationGas: 50_000,
+    maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
+    maxPriorityFeePerGas: hre.ethers.parseUnits("5", "gwei"),
+    paymasterAndData: "0x",
+    signature: "0x"
+  };
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
