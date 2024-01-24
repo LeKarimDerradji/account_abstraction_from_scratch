4ea1d834f656fbdafa49ec4a9ab193896b35dc0b
LeKarimDerradji <karim.derradji.pro@gmail.com>
2024-01-24 23:16:35 +0100

    deployment_script

--- scripts/deployPM.js
+++ scripts/deployPM.js
@@ -7,11 +7,21 @@
 const hre = require("hardhat");
 
 async function main() {
+  const af = await hre.ethers.deployContract("AccountFactory");
   const ep = await hre.ethers.deployContract("EntryPoint");
+  const pm = await hre.ethers.deployContract("Paymaster");
+
+  await af.waitForDeployment();
 
   await ep.waitForDeployment();
 
+  await pm.waitForDeployment();
+
+  console.log(`AccountFactory deployed to ${af.target}`);
+
   console.log(`EntryPoint deployed to ${ep.target}`);
+
+  console.log(`Paymaster deployed to ${pm.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- scripts/test.js
+++ scripts/test.js
@@ -7,14 +7,13 @@
 const hre = require("hardhat");
 const ACCOUNT_ADDRESS = "0x856e4424f806D16E8CBC702B3c0F2ede5468eae5";
 async function main() {
-  
-  const EP_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
-  const AF_ADDRESS = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
-  const EP_CODE = await hre.ethers.provider.getCode(EP_ADDRESS)
-  const AF_CODE = await hre.ethers.provider.getCode(AF_ADDRESS)
-  const Account = await hre.ethers.getContractAt("Account",ACCOUNT_ADDRESS);
+  const EP_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+  const AF_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const PM_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  const EP_CODE = await hre.ethers.provider.getCode(EP_ADDRESS);
+  const AF_CODE = await hre.ethers.provider.getCode(AF_ADDRESS);
+  const Account = await hre.ethers.getContractAt("Account", ACCOUNT_ADDRESS);
   const count = await Account.count();
-
 
   console.log("Count is at: ", count);
 
