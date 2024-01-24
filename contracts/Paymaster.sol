5b40dee09ce2f2b55be631bee59a3e7aac40bf7b
LeKarimDerradji <karim.derradji.pro@gmail.com>
2024-01-24 23:11:42 +0100

    simple_paymaster

---
+++ contracts/Paymaster.sol
@@ -0,0 +1,15 @@
+import "@account-abstraction/contracts/interfaces/IPaymaster.sol";
+
+contract Paymaster is IPaymaster {
+    function validatePaymasterUserOp(UserOperation calldata, bytes32, uint256)
+        external
+        pure
+        override
+        returns (bytes memory context, uint256 validationData)
+    {
+        context = new bytes(0);
+        validationData = 0;
+    }
+
+    function postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) external override {}
+}
